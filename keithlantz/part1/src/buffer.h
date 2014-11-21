c4a3b6e02c404b6a21d508388494a0373ac32512
lenxyang <lenxyang@gmail.com>
2014-11-21 12:21:46 +0800

    .

---
+++ keithlantz/part1/src/buffer.h
@@ -0,0 +1,20 @@
+#ifndef BUFFER_H
+#define BUFFER_H
+
+#include <sstream>
+#include <fstream>
+#include <GL/glew.h>
+#include <GL/gl.h>
+
+class cBuffer {
+  private:
+	int width, height;
+	unsigned char *buffer;
+  protected:
+  public:
+	cBuffer(const int WIDTH, const int HEIGHT);
+	void save(bool video);
+	~cBuffer();
+};
+
+#endif
