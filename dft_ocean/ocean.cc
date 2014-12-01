fbea445e647f1d4eb7ccca7cbe7ef4f82f4cefd4
lenxyang <lenxyang@gmail.com>
2014-12-01 15:54:45 +0800

    .

--- dft_ocean/ocean.cc
+++ dft_ocean/ocean.cc
@@ -4,6 +4,20 @@ const float Ocean::kG = 9.8;
 
 void Ocean::Init() {
   h_tile_.reset(new Complex[kDim * kDim]);
-  for (int i = 0; i <  N + 1; ++i) {
+  for (int i = 0; i < tile_.GetGridLineNum(); ++i) {
+    for (int j= 0; j < tile_.GetGridLineNum(); ++j) {
+      int index = i * tile_.GetGridLineNum() + j;
+      complex htilde0 = CalcHTilde0(i, j);
+      complex htilde0mk_conj = CalcHTilde0(-i, -j).conj();
+    }
   }
+}
+
+float Ocean::phillips(int kn, int km) {
+}
+
+complex Ocean::CalcHTilde0(int kn, int km) {
+}
+
+complex Ocean::CalcHTilde(float t, int n, int m) {
 }
--- dft_ocean/ocean.h
+++ dft_ocean/ocean.h
@@ -2,12 +2,16 @@
 
 #include "base/basictypes.h"
 #include "azer/render/render.h"
-#include "azer/util/tile.h"
+#include "azer/sandbox/water/dft_ocean/tile.h"
 
 #include <complex>
 
 class Ocean {
  public:
+  Ocean(int level)
+      : tile_(level) {
+  }
+
   typedef std::complex<float> complex;
   void Init();
  private:
@@ -16,6 +20,9 @@ class Ocean {
   complex CalcHTilde0(int n, int m);
   complex CalcHTilde(float t, int n, int m);
 
+  // wind's volumn and direction, use vector2 for [x, z]
+  azer::Vector2 wind_;
+  Tile tile_;
   std::unique_ptr<complex[]> h_tilde_;
   static const float kG;
   const float kLength;
